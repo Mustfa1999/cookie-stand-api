6659e22c958f2d03bc87713b743ea9a27d41dca3
Mustfa Ayyed Alhasanat <mustfaaayyed@gmail.com>
2022-06-01 23:34:32 +0300

    first draft

--- project/settings.py
+++ project/settings.py
@@ -34,18 +34,12 @@ environ.Env.read_env()
 
 ENVIRONMENT = env.str("ENVIRONMENT")
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 SECRET_KEY = env.str("SECRET_KEY")
 
 DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = tuple(env.list("ALLOWED_HOSTS"))
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
 # Application definition
 
 INSTALLED_APPS = [
